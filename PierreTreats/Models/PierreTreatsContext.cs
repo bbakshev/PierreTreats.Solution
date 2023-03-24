d5ff498ed3e0bbe3f76aa9a86ad43e8f6c07ea69
Brishna Bakshev <b.bakshev@gmail.com>
2023-03-24 09:55:23 -0700

    added the code for the context and the manytomany treatflavor

---
+++ PierreTreats/Models/PierreTreatsContext.cs
@@ -0,0 +1,13 @@
+using Microsoft.EntityFrameworkCore;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+
+namespace PierreTreats.Models
+{
+    public class PierreTreatsContext : IdentityDbContext<ApplicationUser>
+    {
+        public DbSet<Treat> Treats { get; set; }
+        public DbSet<Flavor> Flavors { get; set; }
+        public DbSet<TreatFlavor> TreatFlavor { get; set; }
+        public PierreTreatsContext(DbContextOptions options) : base(options) { }
+    }
+}
